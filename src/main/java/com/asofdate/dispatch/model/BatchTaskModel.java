08c1ac33599b94f8852772875b163980faac8ff9
hzwy23 <hzwy23@163.com>
2017-06-14 19:46:19 +0800

    add dispatch running monitoring

--- src/main/java/com/asofdate/dispatch/model/BatchTaskModel.java
+++ src/main/java/com/asofdate/dispatch/model/BatchTaskModel.java
@@ -4,4 +4,31 @@ package com.asofdate.dispatch.model;
  * Created by hzwy23 on 2017/6/14.
  */
 public class BatchTaskModel {
+    public String batchId;
+    public String jobId;
+    public String status;
+
+    public String getBatchId() {
+        return batchId;
+    }
+
+    public void setBatchId(String batchId) {
+        this.batchId = batchId;
+    }
+
+    public String getJobId() {
+        return jobId;
+    }
+
+    public void setJobId(String jobId) {
+        this.jobId = jobId;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
 }
